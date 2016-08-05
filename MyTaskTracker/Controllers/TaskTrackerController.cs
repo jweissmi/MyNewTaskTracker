fc723de9c5ef00bbb0d9d2efdac1ec0dc5d08c84
Jim Weiss <jweiss@motor.com>
2016-08-04 20:40:56 -0400

    Commit

---
+++ MyTaskTracker/Controllers/TaskTrackerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyTaskTracker.Models;
+
+namespace MyTaskTracker.Controllers
+{
+    public class TaskTrackerController : Controller
+    {
+        TaskTrackerDataContext db = new TaskTrackerDataContext();
+
+        // GET: TaskTracker
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult GetTaskList()
+        {
+            var user = User.Identity.Name;
+
+            var taskList = TaskTracker.GetTasksByUser(user, db);
+
+            return View(taskList);
+        }
+    }
+}
---
+++ MyTaskTracker/Models/TaskTracker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyTaskTracker.Models
+{
+    public partial class TaskTracker
+    {
+        public static IEnumerable<TaskTracker> GetTasksByUser(string usrName, TaskTrackerDataContext db)
+        {
+            var usrTask = from tskUsr in db.TaskOwners
+                           join tsk in db.TaskLists on tskUsr.TaskId equals tsk.TaskID
+                           where tskUsr.UserName == usrName
+                           select tsk;
+
+            return usrTask;
+        }
+    }
+}
