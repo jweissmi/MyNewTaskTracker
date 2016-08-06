88bf7f36e70829cc65d31cbadfdb6f67300bbe14
Jim Weiss <jweiss@motor.com>
2016-08-06 13:44:07 -0400

    Update 1

--- MyTaskTracker/Controllers/TaskTrackerController.cs
+++ MyTaskTracker/Controllers/TaskTrackerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MyTaskTracker.Models;
+using System.Net;
 
 namespace MyTaskTracker.Controllers
 {
@@ -23,9 +24,28 @@ namespace MyTaskTracker.Controllers
         {
             var user = User.Identity.Name;
 
-            var taskList = TaskTracker.GetTasksByUser(user, db);
+            var taskList = TaskList.GetTasksByUser(user, db);
 
             return View(taskList);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult Create([Bind(Include = "TaskName")] TaskList tasklist)
+        {
+            if (ModelState.IsValid)
+            {
+                TaskList.CreateTask(tasklist, User.Identity.Name, db);
+                return RedirectToAction("GetTaskList");
+            }
+            return View(tasklist);
         }
     }
 }
---
+++ MyTaskTracker/Models/TaskList.cs
@@ -0,0 +1,38 @@
+﻿using MyTaskTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyTaskTracker.Models
+{
+    public partial class TaskList
+    {
+        public static IEnumerable<TaskList> GetTasksByUser(string usrName, TaskTrackerDataContext db)
+        {
+            var usrTask = from tskUsr in db.TaskOwners
+                          join tsk in db.TaskLists on tskUsr.TaskId equals tsk.TaskID
+                          where tskUsr.UserName == usrName
+                          select tsk;
+
+            return usrTask;
+        }
+
+        public static void CreateTask(TaskList tasklist, string usrName, TaskTrackerDataContext db)
+        {
+            db.TaskLists.InsertOnSubmit(tasklist);
+            db.SubmitChanges();
+
+            var id = (from t in db.TaskLists
+                      where t.TaskName == tasklist.TaskName
+                      select t.TaskID).FirstOrDefault();
+
+            TaskOwner tskUsr = new Models.TaskOwner();
+            tskUsr.UserName = usrName;
+            tskUsr.TaskId = id;
+
+            db.TaskOwners.InsertOnSubmit(tskUsr);
+            db.SubmitChanges();
+        }
+    }
+}
